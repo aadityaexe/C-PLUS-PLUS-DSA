a5c4baf75ed9cc441f031fc8cfce244921a9ad26
adityaexe <kmradi2002@gmail.com>
2025-08-21 08:36:10 +0530

    2248. Intersection of Multiple Arrays leetcode question solution

---
+++ DSA/2248. Intersection of Multiple Arrays.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+  vector<int> intersection(vector<vector<int>> &nums)
+  {
+    int n = nums.size();
+    vector<int> ans;
+    unordered_map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < nums[i].size(); j++)
+      {
+        mp[nums[i][j]]++;
+      }
+    }
+    for (auto it : mp)
+    {
+      if (it.second == n)
+        ans.push_back(it.first);
+    }
+    sort(ans.begin(), ans.end());
+    return ans;
+  }
+};
