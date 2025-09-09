7b33247cac57732d6d56f8b5f8b3913dbb3a5afc
adityaexe <kmradi2002@gmail.com>
2025-09-09 22:08:41 +0530

    Remove unused variable 'temp' in isPalindrome method

--- DSA/9. Palindrome Number.java
+++ DSA/9. Palindrome Number.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
         if(x<0) return false;
-        int temp=0,sum=0,num=x;
+        int sum=0,num=x;
         while (num!=0){
             sum= sum*10+num%10;
             num=num/10;
