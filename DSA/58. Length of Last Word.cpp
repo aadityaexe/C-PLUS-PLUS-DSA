6e7b3f332a0ae4df34180c51c89d49560b589b8a
adityaexe <kmradi2002@gmail.com>
2025-10-04 21:07:39 +0530

    Add solution for finding the length of the last word in a string

---
+++ DSA/58. Length of Last Word.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+  int lengthOfLastWord(string s)
+  {
+    int end = s.size() - 1;
+    int count = 0;
+    while (end >= 0 && s[end] == ' ')
+    {
+      end--;
+    }
+    while (end >= 0 && s[end] != ' ')
+    {
+      count++;
+      end--;
+    }
+    return count;
+  }
+};
