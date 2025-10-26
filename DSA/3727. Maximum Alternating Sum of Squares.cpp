8312a2cd84fab77253f56a7c5fa4086385b100e5
adityaexe <kmradi2002@gmail.com>
2025-10-26 23:03:50 +0530

    3727. Maximum Alternating Sum of Squares leetcode question solution

---
+++ DSA/3727. Maximum Alternating Sum of Squares.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+  long long maxAlternatingSum(vector<int> &nums)
+  {
+    // sort(nums.begin(),nums.end(),[](int a, int b){
+    //     return abs(a)> abs(b);
+    // });
+    vector<long long> ok;
+
+    for (int x : nums)
+      ok.push_back(1ll * x * x);
+
+    sort(ok.begin(), ok.end());
+    long long z = 0;
+    int n = ok.size();
+    for (int i = 0; i < n; i++)
+    {
+      if (i % 2 == 0)
+        z += ok[n - 1 - i / 2];
+      else
+        z -= ok[i / 2];
+    }
+
+    return z;
+  }
+};
