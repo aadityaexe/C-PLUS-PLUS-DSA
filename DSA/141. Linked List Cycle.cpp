f728f7d58308168574d2677d1bb11ebe8884c72e
adityaexe <kmradi2002@gmail.com>
2025-11-08 21:20:37 +0530

    141. Linked List Cycle leetcode question solution

---
+++ DSA/141. Linked List Cycle.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+  bool hasCycle(ListNode *head)
+  {
+
+    ListNode *one = head;
+    ListNode *two = head;
+
+    while (two != nullptr && two->next != nullptr)
+    {
+      one = one->next;
+      two = two->next->next;
+
+      if (one == two)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+};
